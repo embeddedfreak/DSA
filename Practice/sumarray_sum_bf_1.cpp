ab7fbd853081793fd21d98cc640a9fbf7848a9df
embeddedfreak <embeddedfreak2020@gmail.com>
2026-05-23 20:20:19 +0530

    Here we use most dumb method
    Where there are 3 loops and TC = O(n^3)
    
    Mark start using outer loop 1
    Mark end using inner lopp 2
    
    Another loop to add from start to end.
    Add up each subarray
    
    Find max

---
+++ Practice/sumarray_sum_bf_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    
+    int n = arr.size();
+    int maxsum = INT_MIN;
+   
+
+    for(int st = 0; st < n; st++) {
+        
+        for(int end = st; end < n; end++) {
+            int sum = 0;
+            for(int i = st; i <= end; i++) {
+                
+                sum+=arr[i];
+		maxsum = maxsum < sum? sum : maxsum;
+            }
+        }   
+    }   
+    cout<<"Maxsum = " << maxsum <<endl;
+}
