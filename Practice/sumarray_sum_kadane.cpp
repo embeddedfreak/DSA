a463fd9d5c6f4b2e2fe511115d16585e34af5cf8
embeddedfreak <embeddedfreak2020@gmail.com>
2026-05-23 20:26:22 +0530

    This is Kadane algorithm
    Which says
    
    Add each element using single loop
    
    If the sum is negative then just ignore the sum
    
    So make sum = 0 everytime if sum = 0;
    
    Keep track of Max
    
    This works well if there are positive and negative numbers aswell

---
+++ Practice/sumarray_sum_kadane.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main()
+{
+	vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+	int n = nums.size();
+	int sum = 0, maxsum = INT_MIN;
+	for(int i = 0; i < n; i++) {
+		sum+=nums[i];
+		maxsum = maxsum < sum ? sum : maxsum;
+		if(sum < 0) {
+			sum = 0;
+		}
+	}
+
+	cout<<"Max Subarray sum = " <<maxsum<<endl;
+
+	return 0;
+}
