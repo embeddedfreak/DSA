32db5d1d8222890ecc3557901d9d9a5b26d014e1
LENOVO <embeddedfreak2020@gmail.com>
2026-05-25 21:55:52 +0530

    Zero to last of the array

---
+++ Practice/zero_to_end_bf.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+
+            if(nums[i] == 0) {
+
+                for(int j = i; j < n-1; j++) {
+                    nums[j] = nums[j+1];
+                }
+
+                nums[n-1] = 0;
+                i--;      // recheck current position
+                n--;      // reduce effective size
+            }
+        }
+    }
+};
